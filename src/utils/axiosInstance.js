1a27285384fac7570937f9438e64101b28f667bc
JeremieAgee <you@example.com>
2024-09-21 10:41:48 -0500

    updated timeout

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const storeAPI = axios.create({
   baseURL: process.env.NEXT_PUBLIC_EXPRESS_STORE_SERVER,
-  timeout: 1000,
+  timeout: 1500,
   headers: { "api-key": process.env.NEXT_PUBLIC_EXPRESS_STORE_API_KEY },
 });
-
-export default storeAPI;
+v
+export default storeAPI
